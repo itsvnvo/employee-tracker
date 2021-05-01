ba330b39ec5766db4abc8ce671c6dfb7e407120e
Vincent Vo <itsvnvo@gmail.com>
2021-04-30 22:33:23 -0500

    fixed syntax error in seed and mysql db, added gif, fixed database name to names in JS

--- employeeTrack.js
+++ employeeTrack.js
@@ -1,9 +1,9 @@
-const mysql2 = require('mysql2');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 
-const connection = mysql2.createConnection({
+const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
@@ -88,7 +88,7 @@ const addDeparment = () => {
             connection.query(
                 'INSERT INTO department SET ?',
                 {
-                    name: answer.addDepart
+                    names: answer.addDepart
                 }, (err) => {
                     if (err) throw err;
                     console.log('Your department was made!');
